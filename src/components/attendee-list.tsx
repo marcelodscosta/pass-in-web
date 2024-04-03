0a8d89d177dee9a2120ba4f516eba7719bba747c
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-04-03 03:51:16 -0300

    attendee-list header

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,5 +1,18 @@
+import { Search } from 'lucide-react';
+
 export function AttendeeList() {
   return (
-    <h1 className="text-2xl mt-5 font-bold">Participantes</h1>
+    <div className="flex items-center gap-5">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+
+      <div className=" flex w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm gap-3">
+        <Search className="size-4 text-emerald-300" />
+        <input
+          type="text"
+          placeholder="Buscar participante"
+          className="w-full bg-transparent outline-none"
+        />
+      </div>
+    </div>
   );
 }
