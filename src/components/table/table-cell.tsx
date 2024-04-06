ee31259923820e521a3a7cc21bf0be66f8d7842f
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-04-06 04:52:36 -0300

    installing tailwind-merge and adjusting table-cell

--- src/components/table/table-cell.tsx
+++ src/components/table/table-cell.tsx
@@ -1,9 +1,13 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 type TableCellProps = {} & ComponentProps<'td'>
 
 export function TableCell({ ...rest }: TableCellProps) {
   return (
-    <td {...rest} className="py-3 px-4 text-sm" />
+    <td
+      {...rest}
+      className={twMerge("py-3 px-4 text-sm", rest.className)}
+    />
   );
 };
